b8d84b98a0a3c4cca017d38ac42733ba1d0e2312
AnthonyGrg <63318864+AnthonyGrg@users.noreply.github.com>
2022-09-30 19:41:13 -0400

    main arguments
    passage des arguments path et seuil dans le main
    le programme applique ensuite egon avec les arguments données en ligne de commande au lancement

--- src.java
+++ src.java
@@ -9,9 +9,17 @@ import java.util.Arrays;
 public class src{
 
 	/*
-	 * TODO
+	 *
 	 */
 	public static void main(String[] args) {
+		if (args.length != 2) {
+			System.out.println("Error : 2 arguments are needed");
+			System.out.println("Please try again with a folder path and a threshold");
+		} else {
+			String path = args[0];
+			int seuil = Integer.parseInt(args[1]);
+			egon(path,seuil);
+		}
 
 	}
 
