eb90b3c6a89d8d5de11bd73e123c4cff234aa449
AnthonyGrg <63318864+AnthonyGrg@users.noreply.github.com>
2022-10-17 20:45:22 -0400

    ajouts au parser
    javaparser pour parse : nom de methodes / contenu des methodes / nombre de d'appels de methodes dans une methode / nombre d'appel de methode total de la classe

--- TP2/metriques/ASTparser.java
+++ TP2/metriques/ASTparser.java
@@ -4,70 +4,125 @@ import com.github.javaparser.ParseException;
 import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.expr.MethodCallExpr;
+import com.github.javaparser.ast.stmt.BlockStmt;
 import com.github.javaparser.ast.visitor.GenericListVisitorAdapter;
 import com.github.javaparser.utils.Log;
-import java.io.BufferedReader;
+
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class ASTparser {
 
-//    public static String getFileContent(String path) {
-//        StringBuilder content = new StringBuilder();
-//        try {
-//            File file = new File(path);
-//            FileReader reader = new FileReader(file);
-//            BufferedReader br = new BufferedReader(reader);
-//            String line;
-//            while((line = br.readLine()) != null) {
-//                content.append(line);
-//                content.append(System.lineSeparator());
-//            }
-//            reader.close();
-//        } catch (IOException e) {
-//            System.out.println(" - Error - ");
-//        }
-//        return content.toString();
-//    }
-
-    public static ArrayList<String> parseMethods() throws ParseException, IOException {
-        // result will store all the methods and their content inside an Arraylist
-        ArrayList<String> result = new ArrayList<>();
+    public static ArrayList<String> parseMethodsNames(String filePath) throws ParseException, IOException {
+        // result will store all the methods names inside an Arraylist
+        ArrayList<String> result;
 
         // JavaParser has a minimal logging class that normally logs nothing.
         // Let's ask it to write to standard out:
         Log.setAdapter(new Log.StandardOutStandardErrorAdapter());
 
         // Then let's parse the file in a compilation unit
-        File file = new File("/Users/anthony/Desktop/IFT3913---TP1-main/src.java");
+        File file = new File(filePath);
         CompilationUnit cu = StaticJavaParser.parse(file);
 
         // Finally the compilation unit take a visitor to parse through the code for each
-        // MethodDeclaration and add them to the result
+        // MethodDeclaration and add their name to the result
         result = (ArrayList<String>) cu.accept(new GenericListVisitorAdapter<String, Void>() {
             @Override
             public ArrayList<String> visit(MethodDeclaration n, Void arg) {
-                ArrayList<String> arr = new ArrayList<>();
+                ArrayList<String> methods = new ArrayList<>();
                 super.visit(n, arg);
-                arr.add(n.toString());
-                return arr;
+                methods.add(n.getName().toString());
+                return methods;
             }
         }, null);
 
         return result;
     }
 
+    public static ArrayList<BlockStmt> parseMethodsContent(String filePath) throws ParseException, IOException {
+        // result will store all the methods contents inside an Arraylist
+        ArrayList<BlockStmt> result;
+
+        // JavaParser has a minimal logging class that normally logs nothing.
+        // Let's ask it to write to standard out:
+        Log.setAdapter(new Log.StandardOutStandardErrorAdapter());
+
+        // Then let's parse the file in a compilation unit
+        File file = new File(filePath);
+        CompilationUnit cu = StaticJavaParser.parse(file);
+
+        // Finally the compilation unit take a visitor to parse through the code for each
+        // MethodDeclaration and add their content (without documentation) to the result
+        result = (ArrayList<BlockStmt>) cu.accept(new GenericListVisitorAdapter<BlockStmt, Void>() {
+            @Override
+            public ArrayList<BlockStmt> visit(MethodDeclaration n, Void arg) {
+                ArrayList<BlockStmt> methods = new ArrayList<>();
+                super.visit(n, arg);
+                if (n.getBody().isPresent()) {
+                    methods.add(n.getBody().get());
+                }
+                return methods;
+            }
+        }, null);
+
+        return result;
+    }
+
+    public static ArrayList<MethodCallExpr> parseMethodCallsInsideAMethod(BlockStmt method) throws ParseException, IOException {
+        // result will store all the method calls inside our BlockStmt
+        ArrayList<MethodCallExpr> result;
+
+        // JavaParser has a minimal logging class that normally logs nothing.
+        // Let's ask it to write to standard out:
+        Log.setAdapter(new Log.StandardOutStandardErrorAdapter());
+
+        // Finally the compilation unit take a visitor to parse through a method (BlockStmt) for each
+        // MethodCallExprand add them to the result
+        result = (ArrayList<MethodCallExpr>) method.accept(new GenericListVisitorAdapter<MethodCallExpr, Void>() {
+            @Override
+            public ArrayList<MethodCallExpr> visit(MethodCallExpr n, Void arg) {
+                ArrayList<MethodCallExpr> methodCalls = new ArrayList<>();
+                super.visit(n, arg);
+                methodCalls.add(n);
+                return methodCalls;
+            }
+        }, null);
+
+        return result;
+    }
+
+    public static int getAllMethodCalls(ArrayList<BlockStmt> methods) throws ParseException, IOException {
+        int result = 0;
+        for (BlockStmt method : methods) {
+            result += parseMethodCallsInsideAMethod(method).size();
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
+        // TESTING
         try {
-            ArrayList<String> methods = parseMethods();
-            System.out.println("Nombre de méthodes : " + methods.size());
-            System.out.println(methods);
+            // Method names in a String format
+            ArrayList<String> methodNames = parseMethodsNames("/Users/anthony/Desktop/IFT3913---TP1-main/src.java");
+            // Method contents in a BlockStmt format to be able to visit them again later
+            ArrayList<BlockStmt> methodContents = parseMethodsContent("/Users/anthony/Desktop/IFT3913---TP1-main/src.java");
+
+            System.out.println("Méthodes : " + methodNames.toString());
+            System.out.println("Nombre de méthodes : " + methodNames.size());
+            System.out.println("Nombre de méthodes appelées : " + getAllMethodCalls(methodContents));
+            System.out.println();
+            for (int i = 0; i < methodNames.size(); i++) {
+                System.out.println("Méthode : " + methodNames.get(i)
+                        + " --- Nombre de méthodes appelées : " + parseMethodCallsInsideAMethod(methodContents.get(i)).size());
+                //System.out.println(methodContents.get(i).toString());
+            }
+
         } catch (IOException | ParseException e) {
             System.out.println("Error");
         }
-
     }
 
 }
