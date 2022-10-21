61ebf0a871cb5471ae1763fea6734647286f10de
LuchinoAllix <60980620+LuchinoAllix@users.noreply.github.com>
2022-10-20 22:55:41 -0400

    coms DC

--- TP2/metriques/DC.java
+++ TP2/metriques/DC.java
@@ -12,19 +12,25 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
-// DC = densité commentaires = CLOC/LOC
+/* Classe qui implémente la méthode dc, utilisée pour obtenir la métrique DC
+* (DC = densité de commentaires) = CLOC/LOC */
 public class DC {
 
+    /* Permet de calculer la métrique dc d'un fichier donné en paramètre
+    * (avec son path en String).*/
     public static float dc(String filePath) throws ParseException, IOException {
 
+        // Listes contenant les différents types de commentaires (grâce à ASTparserComments)
         ArrayList<LineComment> lineComments = ASTparserComments.parseLineComments(filePath);
         ArrayList<BlockComment> blockComments =  ASTparserComments.parseBlockComments(filePath);
         ArrayList<JavadocComment> javadocComments = ASTparserComments.parseJavadocComments(filePath);
+        // Compteurs de lignes de commentaires et de code
         int comments = 0;
         int lines = 0;
 
         comments += lineComments.size();
 
+        // On compte le nombre de lignes de code de commentaire des blocks.
         for(BlockComment blockComment : blockComments){
             comments++;
             String comment = blockComment.toString();
@@ -33,6 +39,7 @@ public class DC {
             }
         }
 
+        // On compte le nombre de lignes de code de commentaire de la javadoc
         for(JavadocComment javadocComment : javadocComments){
             comments++;
             String comment = javadocComment.toString();
@@ -41,6 +48,7 @@ public class DC {
             }
         }
 
+        // On compte le nombre de lignes non vides (un espace ou tabulation n'est pas comptabilisée vide.
         try {
             File file = new File(filePath);
             FileReader reader = new FileReader(file);
@@ -54,7 +62,7 @@ public class DC {
             assert true; // Pour ne rien faire
         }
 
-        if (lines == 0) return 0; // Si le fichier est vide
+        if (lines == 0) return 0; // Si le fichier est vide, le ratio est 0.
 
         return (float) comments/lines ;
     }
