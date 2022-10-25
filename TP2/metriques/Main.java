f8e4b0e352f6be7290971616dd64d48d4444ba0c
AnthonyGrg <63318864+AnthonyGrg@users.noreply.github.com>
2022-10-25 03:33:53 -0400

    Documentation / préparation q2,q3,q4

--- TP2/metriques/Main.java
+++ TP2/metriques/Main.java
@@ -4,35 +4,35 @@ import com.github.javaparser.ParseException;
 
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Hashtable;
 import java.util.Objects;
 
 
 /* Fichier principal du TP2.
 * C'est depuis ici que le programme se lance.
-* Ici que le chemin du dossier à analyser est transformé en csv puis donné
+* Le chemin du dossier à analyser est transformé en csv puis donné
 * aux différentes classes pour obtenir les métriques qui seront à leur tour
-* ajoutées dans le fichier csv qui sera produit.*/
+* ajouté dans le fichier csv qui sera produit.
+* Finalement applique les calculs grâce aux métriques pour répondre aux questions du GQM*/
 public class Main {
 
-    /* Récupéréé du TP1, permet de trouver tous les fichiers java et les mets
-    * sous forme de String.*/
+    /* Récupéré du TP1, permet de trouver tous les fichiers java et les mets
+    * sous forme de String en format csv (filePath, module, filename).*/
     public static String jls(String path) {
         File file = new File(path);
         return jlsRec(file,file.getName());
     }
 
-    /* Partie récursive de jls (également récupérée du TP1 */
+    /* Partie récursive de jls (également récupérée du TP1) */
     public static String jlsRec(File path, String module) {
         StringBuilder temp = new StringBuilder();
 
         // On regarde tous les fichiers :
         for (final File file : Objects.requireNonNull(path.listFiles())) {
 
-            // Si on regarde un dossier on fait un appel récursif
+            // Si on regarde un dossier, on fait un appel récursif
             if (file.isDirectory()) {
                 temp.append(jlsRec(file, module + "." + file.getName()));
-            } else if (file.isFile()) { // Si c'est un fichier '.java' on prend les informations nécessaires
+            } else if (file.isFile()) { // Si c'est un fichier '.java', on prend les informations nécessaires
                 int len = file.getName().length();
                 if (len > 5 && file.getName().substring(len-5).equals(".java")){
                     temp.append(file.getPath()).append(",")
@@ -44,26 +44,27 @@ public class Main {
         return temp.toString();
     }
 
-
-    /* Separates methods to calculate csec once because csec already outputs the csv
-    * file augmented with csec values */
-    public static String addCSECToCSV(String csv) {
-        return CSEC.csec(csv);
-    }
-
     /* Separatse methods to calculate PMNT and TPC once because pmnt already outputs the
-    * csv file augmented with pmnt and tpc values */
+     * csv file augmented with pmnt and tpc values */
     public static String addPMNTToCSV(String csv){
         return PMNT.pmnt(csv);
     }
 
-    /* Méthode qui permet d'ajouter toutes les métriques restante au string csv.*/
+    /* Separates methods to calculate csec once because csec already outputs the csv
+    * file augmented with csec values  */
+    public static String addCSECToCSV(String csv) {
+        return CSEC.csec(csv);
+    }
+
+    /* Méthode finale qui permet d'ajouter toutes les métriques restantes au string csv ainsi
+    *  qu'une ligne de titre pour chaque colonne :
+    *  (filePath, module, fileName, pmnt, tpcbis, csec, wmc, rfc, dc, loc, cloc). */
     public static String addMetricsToCSV(String csv) {
         // On ajoute les noms des colonnes
         StringBuilder result = new StringBuilder("filePath,module,fileName,pmnt,tpcbis,csec,wmc,rfc,dc,loc,cloc\n");
         String[] csvEntries = csv.split("\n");
 
-        // Pour chaque fichier on calcule la métrique et on la rajoute au fichier
+        // Pour chaque fichier, on calcule la métrique et on la rajoute au fichier
         for (String csvEntry : csvEntries) {
             try {
                 String filePath = csvEntry.split(",")[0];
@@ -83,7 +84,7 @@ public class Main {
         return result.toString();
     }
 
-    /* Crée le fichier csv (ou remplace celui déjà existant) */
+    /* Crée le fichier csv (ou remplace celui déjà existant) et le remplie avec la String donné en argument */
     public static void writeToFile(String content, String fileName) {
         try {
             // Création nouveau fichier ou remplacement de l'ancien
@@ -100,6 +101,7 @@ public class Main {
         }
     }
 
+    /* Retire les fichiers test du csv pour la suite des calculs qui répondent aux questions */
     public static String removeTestLinesFromCsv(String csv) {
         StringBuilder result = new StringBuilder("");
         String[] csvEntries = csv.split("\n");
@@ -114,13 +116,14 @@ public class Main {
 
     }
 
-    public static Boolean egonQ1(String csv){
+    public static Boolean answerQ1(String csv) {
+        // Init deux tableaux de complexité et cloc
         ArrayList<Float> compl = new ArrayList<>();
         ArrayList<Float> cloc = new ArrayList<>();
-
-        int cnt = 0;
-
+        // csvSplit contient chaque ligne du csv
         String[] csvSplit = csv.split("\n");
+
+        // Pour chaque ligne du csv ajouter WMC+RFC à compl[] et cloc à cloc[]
         for (int i = 1; i < csvSplit.length; i++) {
             String[] line = csvSplit[i].split(",");
             float complF = Float.parseFloat(line[6])+ Float.parseFloat(line[7]);
@@ -128,46 +131,72 @@ public class Main {
             compl.add(complF);
             cloc.add(clocF);
         }
-        int size = compl.size();
 
+        // Trie les tableaux compl[] et cloc[] dans les tableaux complS[] et clocS[] respectivement
         ArrayList<Float> complS = new ArrayList<>(compl);
         complS.sort(null);
         ArrayList<Float> clocS = new ArrayList<>(cloc);
         clocS.sort(null);
-        System.out.println(complS);
-        System.out.println(clocS);
+        //System.out.println(complS);
+        //System.out.println(clocS);
 
-        for (int i =0;i<size;i++) {
+        // TODO commentaires
+        int count = 0;
+        int size = compl.size();
+
+        // TODO commentaires
+        for (int i = 0; i < size; i++) {
+
             int complIndexMin = 0;
             int complIndexMax = 0;
             Float clocMin;
             Float clocMax;
+
             for (int j = 0; j < size; j++) {
                 if (Objects.equals(compl.get(i), complS.get(j))) {
                     complIndexMin = j;
                     complIndexMax = j;
                     // Si jamais il y a plusieurs fois la même valeur
-                    while(j<size-1 && Objects.equals(compl.get(i),complS.get(j+1))){
+                    while (j < (size - 1) && Objects.equals(compl.get(i),complS.get(j+1))) {
                         complIndexMax++;
                         j++;
                     }
                     break;
                 }
             }
-            int indexClocMin = complIndexMin - (size)/5;
-            if(indexClocMin < 0) indexClocMin = 0;
-            clocMin = clocS.get(indexClocMin) ;
-            int indexClocMax = complIndexMax + (size)/5;
-            if(indexClocMax > size) indexClocMax = size-1;
+
+            int indexClocMin = complIndexMin - (size) / 5;
+            if (indexClocMin < 0) indexClocMin = 0;
+            clocMin = clocS.get(indexClocMin);
+            int indexClocMax = complIndexMax + (size) / 5;
+            if (indexClocMax > size) indexClocMax = size - 1;
             clocMax = clocS.get(indexClocMax);
-            System.out.println(compl.get(i) +" : "+clocMin+" - "+ cloc.get(i) +" - "+clocMax);
+            // Si cloc est entre le min/max alors l'ajouter au compteur
             if( cloc.get(i) > clocMax || cloc.get(i) < clocMin){
-                cnt++;
+                count++;
             }
+
+            System.out.println("complexity : " + compl.get(i) // TODO remove
+                    + " -> clocMin : " + clocMin
+                    + " -> cloc : " + cloc.get(i)
+                    + " -> clocMax : " + clocMax);
         }
-        System.out.println(cnt);
-        System.out.println(size);
-        return cnt <= compl.size() / 10;
+        System.out.println("compteur : " + count); // TODO remove
+        System.out.println("taille : " + size); // TODO remove
+
+        return count <= compl.size() / 10; // TODO commentaire explication calcul resultat
+    }
+
+    public static Boolean answerQ2(String csv) {
+        return false;
+    }
+
+    public static Boolean answerQ3(String csv) {
+        return false;
+    }
+
+    public static Boolean answerQ4(String csv) {
+        return false;
     }
 
     /* Emplacement du lancement du programme du TP2 */
@@ -179,8 +208,6 @@ public class Main {
         } else {
             // path of the folder
             String folderPath = args[0];
-            //String folderPath = "/Users/anthony/Desktop/jfreechart-master";
-            //String folderPath2 = "C:\\Users\\luchi\\Desktop\\jfreechart";
 
             // create a String in csv format with filepath,module,fileName
             String csv = jls(folderPath);
@@ -202,8 +229,13 @@ public class Main {
 
             // write result to a csv file
             writeToFile(csv, "output.csv");
-            System.out.println(egonQ1(csv));
             System.out.println("Le fichier output.csv a bien été crée.");
+
+            // Question 1 du GQM :
+            System.out.println("Réponse question 1 GQM : " + answerQ1(csv));
+            System.out.println("Réponse question 2 GQM : " + answerQ2(csv));
+            System.out.println("Réponse question 3 GQM : " + answerQ3(csv));
+            System.out.println("Réponse question 4 GQM : " + answerQ4(csv));
         }
     }
 	
