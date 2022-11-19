eeb98e168045776203dcb87c951e88216d76e045
LuchinoAllix <60980620+LuchinoAllix@users.noreply.github.com>
2022-11-18 21:22:42 -0500

    plot titles + suppression points extrêmes nocom/NCLOC

--- TP3/main.py
+++ TP3/main.py
@@ -50,17 +50,27 @@ def bar(array):
             dico[val] = 1
     return dico
 
+
 # Pour obtenir les graphiques de distribution
 
 plt.figure("Nocom_Bar")
+plt.title("NoCom Distribution")
+plt.xlabel("Valeurs")
+plt.ylabel("Quantité")
 dico_NoCom = bar(NoCom)
 plt.bar(list(dico_NoCom.keys()), list(dico_NoCom.values()))
 
 plt.figure("NCLOC_Bar")
+plt.title("NCLOC Distribution")
+plt.xlabel("Valeurs")
+plt.ylabel("Quantité")
 dico_NCLOC = bar(NCLOC)
 plt.bar(list(dico_NCLOC.keys()), list(dico_NCLOC.values()))
 
 plt.figure("DCP_Bar")
+plt.title("DCP Distribution")
+plt.xlabel("Valeurs")
+plt.ylabel("Quantité")
 dico_DCP = bar(DCP)
 plt.bar(list(dico_DCP.keys()), list(dico_DCP.values()))
 
@@ -68,15 +78,21 @@ plt.bar(list(dico_DCP.keys()), list(dico_DCP.values()))
 
 # Affichage des plots de boites à moustaches
 plt.figure("NoCom")
+plt.title("Boîte à moustache NoCom")
+plt.ylabel("Valeurs")
 bp_NoCom = plt.boxplot(NoCom, showmeans=True)
 
 plt.figure("NCLOC")
+plt.title("Boîte à moustache NCLOC")
+plt.ylabel("Valeurs")
 bp_NCLOC = plt.boxplot(NCLOC, showmeans=True)
 
 plt.figure("DCP")
+plt.title("Boîte à moustache DCP")
+plt.ylabel("Valeurs")
 bp_DCP = plt.boxplot(DCP, showmeans=True)
 
-plt.figure("All")
+plt.figure("All")  # obligé d'être affichée car facilite les calculs
 
 # Affichage des données
 # source : https://towardsdatascience.com/how-to-fetch-the-exact-values-from-a-boxplot-python-8b8a648fc813
@@ -119,6 +135,9 @@ for i in range(len(categories)):
 # NoCom/NCLOC
 
 plt.figure("NoCom/NCLOC")
+plt.title("NoCom par rapport à NCLOC")
+plt.xlabel("NoCom")
+plt.ylabel("NCLOC")
 
 # Tri des données
 NoCom_NCLOC_S = sorted(NoCom_NCLOC)
@@ -126,8 +145,9 @@ NoCom_S = []
 NCLOC_S = []
 
 for i in range(len(NoCom_NCLOC_S)):
-    NoCom_S.append(NoCom_NCLOC_S[i][0])
-    NCLOC_S.append(NoCom_NCLOC_S[i][1])
+    if NoCom_NCLOC_S[i][1] < 1400:
+        NoCom_S.append(NoCom_NCLOC_S[i][0])
+        NCLOC_S.append(NoCom_NCLOC_S[i][1])
 
 # regression linéaire
 coef = np.polyfit(NoCom_S, NCLOC_S, 1)
@@ -142,6 +162,9 @@ plt.plot(NoCom_S, NCLOC_S, '.', NoCom_S, poly1d_fn(NoCom_S), '--k')
 # NoCom/DCP
 
 plt.figure("NoCom/DCP")
+plt.title("NoCom par rapport à DCP")
+plt.xlabel("NoCom")
+plt.ylabel("DCP")
 
 # Tri des données
 NoCom_DCP_S = sorted(NoCom_DCP)
@@ -157,18 +180,18 @@ poly1d_fn = np.poly1d(coef)
 
 print(f'\033[1m{"NoCom/NCLOC :"}\033[0m')
 print("\t m = " + str(coef[0]))
-print("\t b = " + str(coef[1])+"\n")
+print("\t b = " + str(coef[1]) + "\n")
 
 plt.plot(NoCom_S1, DCP_S, '.', NoCom_S1, poly1d_fn(NoCom_S1), '--k')
 
-plt.show() # Affichage des plots
+plt.show()  # Affichage des plots
 
 ### Spearman ###
 
 # Appel à spearmanr de scipy
 
 print(f'\033[1m{"NoCom/NCLOC :"}\033[0m')
-print("p=" + str(spearmanr(NoCom, NCLOC)[0])+"\n")
+print("p=" + str(spearmanr(NoCom, NCLOC)[0]) + "\n")
 
 print(f'\033[1m{"NoCom/NCLOC :"}\033[0m')
-print("p=" + str(spearmanr(NoCom, DCP)[0])+"\n")
+print("p=" + str(spearmanr(NoCom, DCP)[0]) + "\n")
