71c27aac3f5a4b6d5257999bac9af6a2b9de1e44
LuchinoAllix <60980620+LuchinoAllix@users.noreply.github.com>
2022-12-13 17:44:35 -0500

    tests working

--- TP4/ExpensesManager/src/test/java/ua/karatnyk/TestCurrencyConvertor.java
+++ TP4/ExpensesManager/src/test/java/ua/karatnyk/TestCurrencyConvertor.java
@@ -1,21 +1,31 @@
 package ua.karatnyk;
 
 import static org.junit.Assert.*;
+import org.junit.Test;
 
 import java.text.ParseException;
+import java.util.Map;
 
 import ua.karatnyk.impl.CurrencyConvertor;
 import ua.karatnyk.impl.CurrencyConversion;
+import ua.karatnyk.impl.OfflineJsonWorker;
 
-import org.junit.Before;
-import org.junit.Test;
+
 
 public class TestCurrencyConvertor {
+
+	private static OfflineJsonWorker offlineJson = new OfflineJsonWorker();
+	private static CurrencyConversion conversion = offlineJson.parser();
+	private static Map<String, Double> rates = conversion.getRates();
 	
 	@Test
-	public void Test1() throws ParseException{
-		CurrencyConversion i = new CurrencyConversion();
-		double d = CurrencyConvertor.convert(30, "EUR", "CAD", i);
-		assertTrue(d==1.0);
+	public void TestBN1() throws ParseException{
+		double amount = 30;
+		String from = "EUR";
+		String to = "CAD";
+		double automatic = CurrencyConvertor.convert(amount, from, to, conversion);
+		double manual = amount*(rates.get(to)/rates.get(from));
+		assertTrue(manual==automatic);
 	}
+	
 }
